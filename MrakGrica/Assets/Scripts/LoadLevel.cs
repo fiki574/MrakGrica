e1a80674b6379354fe615c2dfff67f6f6bd0a75c
fiki574 <fiki8796@hotmail.com>
2018-01-14 00:17:23 +0100

    Drugi level
    - Dodani collideri
    - Dodano load-anje kada se dođe do kraja prvog levela
    - Dodan "template" za load-anje trećeg levela, kasnije završiti

---
+++ MrakGrica/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public string ID;
+    public Rigidbody2D Player;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (ID == "LoadLevel2")
+            SceneManager.LoadScene("Level2", LoadSceneMode.Single);
+        else if (ID == "LoadLevel3")
+            Debug.Log("Level 3 not yet present.");
+    }
+}
