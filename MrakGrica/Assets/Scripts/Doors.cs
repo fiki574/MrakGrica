d97cbfe6081cd07e4aa38c84157eb77b265f9387
fiki574 <fiki8796@hotmail.com>
2018-01-13 14:08:21 +0100

    Još stvarčica
    - Dodan basic movement (Renato)
    - Dodano teleportiranje na odgovarajuća vrata/kat pritiskom tipke E kada
    je igrač blizu vrata
    - Dodan polygon collider za stepenice ispred vrata

---
+++ MrakGrica/Assets/Scripts/Doors.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public string ID;
+    public Rigidbody2D Player;
+    private bool CanTeleport;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        CanTeleport = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        CanTeleport = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            if (CanTeleport)
+            {
+                switch (ID)
+                {
+                    case "KPV1":
+                        {
+                            Player.transform.position = new Vector2(18.5f, -5.9f);
+                            break;
+                        }
+
+                    case "K0V1":
+                        {
+                            Player.transform.position = new Vector2(18.5f, -1.2f);
+                            break;
+                        }
+
+                    case "K0V2":
+                        {
+                            Player.transform.position = new Vector2(18.5f, -12.6f);
+                            break;
+                        }
+
+                    case "K1V1":
+                        {
+                            Player.transform.position = new Vector2(18.5f, 3.75f);
+                            break;
+                        }
+
+                    case "K1V2":
+                        {
+                            Player.transform.position = new Vector2(9.2f, -5.8f);
+                            break;
+                        }
+
+                    case "K2V1":
+                        {
+                            Player.transform.position = new Vector2(0.0f, 8.5f);
+                            break;
+                        }
+
+                    case "K2V2":
+                        {
+                            Player.transform.position = new Vector2(0.0f, -1.15f);
+                            break;
+                        }
+
+                    case "K3V1":
+                        {
+                            Player.transform.position = new Vector2(0.0f, 3.7f);
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ MrakGrica/Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 100;
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector2 tempVect = new Vector2(h, v);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        rb.MovePosition((Vector2)rb.transform.position + tempVect);
+    }
+}
