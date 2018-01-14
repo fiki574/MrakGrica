739d5474a0dd6556ff8d2029007282b43c0d3d3d
rkudec <renato.kudec@yahoo.com>
2018-01-14 11:44:53 +0100

    kamera
    camera clamp, tweak na svijetlu

--- MrakGrica/Assets/Scripts/CameraController.cs
+++ MrakGrica/Assets/Scripts/CameraController.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public GameObject player;       //Public variable to store a reference to the player game object
+    public Transform player;  
+    public float minX, minY, maxX, maxY;
 
 
-    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
-
-    // Use this for initialization
     void Start()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        player = GameObject.Find("Player").transform;
     }
 
-    // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        float x = Mathf.Clamp(player.position.x, minX, maxX);
+        float y = Mathf.Clamp(player.position.y, minY, maxY);
+        transform.position = new Vector2(x, y);
     }
 }
