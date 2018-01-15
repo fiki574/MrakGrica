10a766998cc064fe00b093069721042082a6eead
rkudec <renato.kudec@yahoo.com>
2018-01-15 18:42:25 +0100

    animacije

--- MrakGrica/Assets/Scripts/Movement.cs
+++ MrakGrica/Assets/Scripts/Movement.cs
@@ -5,9 +5,17 @@ public class Movement : MonoBehaviour
     public Rigidbody2D Player;
     private float speed = 5;
 
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         float h = Input.GetAxis("Horizontal"), v = Input.GetAxis("Vertical");
+        animator.SetFloat("speed", Mathf.Abs(h));
         Vector2 tempVect = new Vector2(h, v);
         tempVect = tempVect.normalized * speed * Time.deltaTime;
         Player.MovePosition((Vector2)Player.transform.position + tempVect);
