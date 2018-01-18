2f7d75ebd2de1b9bf7e9b3ccd437d7f8a941212b
fiki574 <fiki8796@hotmail.com>
2018-01-18 18:08:33 +0100

    "Overhaul"
    - Popravljeni teleporti
    - Maknuto fadeanje Player spritea
    - Dodano fadeanje od 2.5s kada igrač prolazi kroz vrata/okna
    - Dodan ključ i zaključana vrata (Renato: treba "light" za njih
    popravit)
    - Dodan ambient sound na 1. level i meni (za probu, poslije mijenjamo)
    - Pojačan ambient sound na 2. i 3. levelu
    - Uzimanje ključa i otvaranja vrata radi, "F" za interakciju sa oba
    objekta
    - Maknuti nepotrebni collideri sa 2. levela
    - Maknut "YOU WON" trigger sa 3. levela na dnu

--- MrakGrica/Assets/Scripts/Doors.cs
+++ MrakGrica/Assets/Scripts/Doors.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Doors : MonoBehaviour
 {
     public string ID;
     public Rigidbody2D Player;
-    public SpriteRenderer Sprite, IzlazSprite, Key;
     public AudioSource DoorsOpen, DoorsClose;
-    public GameObject DestroyableCollider;
-    private bool CanTeleport = false, Pressed = false, Faded = false;
-    private float minimum = 0.0f, maximum = 1f, speed = 1.5f, threshold = 0.05f;
+    private bool CanTeleport = false;
+    private GUIStyle m_BackgroundStyle = new GUIStyle(); 
+    private Texture2D m_FadeTexture;               
+    private Color m_CurrentScreenOverlayColor = new Color(0, 0, 0, 0);  
+    private Color m_TargetScreenOverlayColor = new Color(0, 0, 0, 0);  
+    private Color m_DeltaColor = new Color(0, 0, 0, 0);
+    private int m_FadeGUIDepth = -1000;
 
     void OnTriggerEnter2D(Collider2D collider)
     {
@@ -25,50 +29,24 @@ public class Doors : MonoBehaviour
         if (Input.GetKeyDown("e"))
             if (CanTeleport)
             {
-                Pressed = true;
                 Player.constraints = RigidbodyConstraints2D.FreezeAll;
-                DoorsOpen.Play();
+                if(DoorsOpen != null)
+                    DoorsOpen.Play();
+                StartFade(Color.black, 2.5f);
+                StartCoroutine(Teleport());
             }
-
-        if (Input.GetKeyDown("f"))
-            if (ID == "Stol")
-                Destroy(Key);
-
-        if (Input.GetKeyDown("g"))
-            if (ID == "Izlaz")
-            {
-                Destroy(DestroyableCollider.GetComponent<Collider2D>());
-                Destroy(IzlazSprite);
-                DoorsOpen.Play();
-            }
-
-        float step = speed * Time.deltaTime;
-        if (Pressed && !Faded)
-        {
-            Sprite.color = new Color(1f, 1f, 1f, Mathf.Lerp(Sprite.color.a, minimum, step));
-            if (Mathf.Abs(Sprite.color.a - minimum) <= threshold)
-            {
-                Faded = true;
-                Teleport();
-            }
-        }
-
-        if (!Pressed && Faded)
-        {
-            Sprite.color = new Color(1f, 1f, 1f, Mathf.Lerp(Sprite.color.a, maximum, step));
-            if (Mathf.Abs(maximum - Sprite.color.a) <= threshold)
-                Faded = false;
-        }
     }
 
-    private void Teleport()
+    IEnumerator Teleport()
     {
+        yield return new WaitForSeconds(2);
+
         if (ID == "KPV1")
-            Player.transform.position = new Vector2(18.5f, -5.9f);
+            Player.transform.position = new Vector2(9.56f, -6.52f);
         else if (ID == "K0V1")
             Player.transform.position = new Vector2(9.04f, -0.52f);
         else if (ID == "K0V2")
-            Player.transform.position = new Vector2(18.5f, -12.6f);
+            Player.transform.position = new Vector2(9.56f, -13.1f);
         else if (ID == "K1V1")
             Player.transform.position = new Vector2(9.01f, 3.98f);
         else if (ID == "K1V2")
@@ -80,20 +58,69 @@ public class Doors : MonoBehaviour
         else if (ID == "K3V1")
             Player.transform.position = new Vector2(-9.54f, 4.47f);
         else if (ID == "K0O1")
-            Player.transform.position = new Vector2(-3.49f, -11.6f);
+            Player.transform.position = new Vector2(2.38f, -11.98f);
         else if (ID == "K-1O1")
-            Player.transform.position = new Vector2(11.66f, -4.53f);
+            Player.transform.position = new Vector2(-12.82f, -18.93f);
         else if (ID == "K-1O2")
-            Player.transform.position = new Vector2(-3.43f, -18.2f);
+            Player.transform.position = new Vector2(2.38f, -5.4f);
         else if (ID == "K-2O1")
-            Player.transform.position = new Vector2(11.82f, -11.4f);
+            Player.transform.position = new Vector2(-12.82f, -11.98f);
         else if (ID == "K-2O2")
-            Player.transform.position = new Vector2(-3.5f, -25.8f);
+            Player.transform.position = new Vector2(-12.8f, -26.31f);
         else if (ID == "K-3O1")
-            Player.transform.position = new Vector2(11.64f, -18.3f);
+            Player.transform.position = new Vector2(2.3f, -18.59f);
 
-        Pressed = false;
         Player.constraints = RigidbodyConstraints2D.FreezeRotation;
-        DoorsClose.Play();
+        if (DoorsClose != null)
+            DoorsClose.Play();
+        yield return new WaitForSeconds(2);
+        Destroy(m_FadeTexture);
+        yield return null;
+    }
+
+    private void Awake()
+    {
+        m_FadeTexture = new Texture2D(1, 1);
+        m_BackgroundStyle.normal.background = m_FadeTexture;
+        SetScreenOverlayColor(m_CurrentScreenOverlayColor);
+    }
+
+    private void OnGUI()
+    {
+        if (m_CurrentScreenOverlayColor != m_TargetScreenOverlayColor)
+        {
+            if (Mathf.Abs(m_CurrentScreenOverlayColor.a - m_TargetScreenOverlayColor.a) < Mathf.Abs(m_DeltaColor.a) * Time.deltaTime)
+            {
+                m_CurrentScreenOverlayColor = m_TargetScreenOverlayColor;
+                SetScreenOverlayColor(m_CurrentScreenOverlayColor);
+                m_DeltaColor = new Color(0, 0, 0, 0);
+            }
+            else
+                SetScreenOverlayColor(m_CurrentScreenOverlayColor + m_DeltaColor * Time.deltaTime);
+        }
+
+        if (m_CurrentScreenOverlayColor.a > 0)
+        {
+            GUI.depth = m_FadeGUIDepth;
+            GUI.Label(new Rect(-10, -10, Screen.width + 10, Screen.height + 10), m_FadeTexture, m_BackgroundStyle);
+        }
+    }
+
+    public void SetScreenOverlayColor(Color newScreenOverlayColor)
+    {
+        m_CurrentScreenOverlayColor = newScreenOverlayColor;
+        m_FadeTexture.SetPixel(0, 0, m_CurrentScreenOverlayColor);
+        m_FadeTexture.Apply();
+    }
+
+    public void StartFade(Color newScreenOverlayColor, float fadeDuration)
+    {
+        if (fadeDuration <= 0.0f)
+            SetScreenOverlayColor(newScreenOverlayColor);
+        else
+        {
+            m_TargetScreenOverlayColor = newScreenOverlayColor;
+            m_DeltaColor = (m_TargetScreenOverlayColor - m_CurrentScreenOverlayColor) / fadeDuration;
+        }
     }
 }
---
+++ MrakGrica/Assets/Scripts/KeyLock.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class KeyLock : MonoBehaviour
+{
+    public Rigidbody2D Player;
+    public GameObject Exit, Key;
+    public AudioSource DoorsOpen;
+    private bool HasKey = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            if (Player.GetComponent<Collider2D>().IsTouching(Exit.GetComponent<Collider2D>()) && HasKey)
+            {
+                Destroy(Exit);
+                DoorsOpen.Play();
+            }
+
+            if (Player.GetComponent<Collider2D>().IsTouching(Key.GetComponent<Collider2D>()) && !HasKey)
+            {
+                Destroy(Key);
+                HasKey = true;
+            }
+        }
+    }
+}
