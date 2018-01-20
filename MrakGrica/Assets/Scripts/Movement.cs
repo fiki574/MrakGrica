a614ed95c0334da51cd04147e11b6c537c4704cf
rkudec <renato.kudec@yahoo.com>
2018-01-21 00:11:26 +0100

    Popravljen movement/dodano pucanje
    -dodan flip i usklađene animacije i speed
    -dodana animacija za pucanje na mouse1 ali jos treba poslozit brzinu i rijesit vracanje u idle stanje
    -treba jos dodat 2 idle stanja sa/bez pistolja

--- MrakGrica/Assets/Scripts/Movement.cs
+++ MrakGrica/Assets/Scripts/Movement.cs
@@ -2,23 +2,63 @@
 
 public class Movement : MonoBehaviour
 {
-    public Rigidbody2D Player;
-    private float speed = 6;
-
+    private Rigidbody2D player;
+    private bool facingRight;
     private Animator animator;
+	private bool attack;
+
+	[SerializeField]
+	private float speed = 4;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        player = GetComponent<Rigidbody2D>();
+        facingRight = true;
     }
 
     void Update()
     {
-        float h = Input.GetAxis("Horizontal"), v = Input.GetAxis("Vertical");
-        animator.SetFloat("speed", Mathf.Abs(h));
-        Vector2 tempVect = new Vector2(h, v);
-        tempVect = tempVect.normalized * speed * Time.deltaTime;
-        Player.MovePosition((Vector2)Player.transform.position + tempVect);
-
+        float h = Input.GetAxis("Horizontal");
+		HandleMovement (h);
+		Flip (h);
+		HandleInput();
+		HandleShoot();
+		Reset ();
     }
+
+	private void HandleMovement(float h)
+    {
+        animator.SetFloat("speed", Mathf.Abs(h));
+        player.velocity = new Vector2(h * speed, player.velocity.y);
+    }
+
+	private void Flip(float h)
+	{
+		if (h > 0 && !facingRight || h < 0 && facingRight) 
+		{
+			facingRight = !facingRight;
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+	}
+
+	private void HandleShoot()
+	{
+		if(attack)
+			animator.SetTrigger("attack");
+	}
+
+	private void HandleInput()
+	{
+		if (Input.GetButtonDown("Fire1"))
+			attack = true;
+	}
+
+	private void Reset()
+	{
+		attack = false;
+	}
+
 }
